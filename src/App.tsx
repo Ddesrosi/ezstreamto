20082a43aa893401551035762937c77bff06ac0b
ddesrosi <desrosiersd1@gmail.com>
2025-04-11 14:29:26 -0400

    11avril4

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ function App() {
     hasLoggedRef.current = true;
 
     try {
-      const response = await fetch('https://api.ipify.org?format=json');
+      const response = await fetch('https://api64.ipify.org?format=json');
       const data = await response.json();
       const ip = data.ip;
 
